3d54e246dd8dd756d7e5d8809734b0fd670085da
KorenPapazian <kpapazian@edenschool.fr>
2022-06-09 16:16:48 +0200

    HTML and JS

--- js/app.js
+++ js/app.js
@@ -13,47 +13,47 @@ const compteur = document.getElementById("compteur");
 
 btn.addEventListener("click", () => {
     let compt = 0;
-  while (compt < 5) {
-    compt++;
-    compteur.innerText = compt;
-  }
+    while (compt < 5) {
+      compt++;
+      compteur.innerText = compt;
+    }
 
-  divp.addEventListener("click", () => {
-    joueur = t[0];
-    if (pfc == joueur) {
-        console.log("Nul");
-    }
-    if (joueur && pfc == t[1]) {
-        console.log("Perdu");
-    }
-    if (joueur && pfc == t[2]) {
-        console.log("Gagner");
-    }
-  });
+    divp.addEventListener("click", () => {
+      joueur = t[0];
+      if (pfc == joueur) {
+          console.log("Nul");
+      }
+      if (joueur && pfc == t[1]) {
+          console.log("Perdu");
+      }
+      if (joueur && pfc == t[2]) {
+          console.log("Gagner");
+      }
+    });
 
-  divf.addEventListener("click", () => {
-    joueur = t[1];
-    if (pfc == joueur) {
-        console.log("Nul");
-    }
-    if (joueur && pfc == t[2]) {
-        console.log("Perdu");
-    }
-    if (joueur && pfc == t[0]) {
-        console.log("Gagner");
-    }
-  });
+    divf.addEventListener("click", () => {
+      joueur = t[1];
+      if (pfc == joueur) {
+          console.log("Nul");
+      }
+      if (joueur && pfc == t[2]) {
+          console.log("Perdu");
+      }
+      if (joueur && pfc == t[0]) {
+          console.log("Gagner");
+      }
+    });
 
-  divc.addEventListener("click", () => {
-    joueur = t[2];
-    if (pfc == joueur) {
-        console.log("Nul");
-    }
-    if (joueur && pfc == t[0]) {
-        console.log("Perdu");
-    }
-    if (joueur && pfc == t[1]) {
-        console.log("Gagner");
-    }
-  });
+    divc.addEventListener("click", () => {
+      joueur = t[2];
+      if (pfc == joueur) {
+          console.log("Nul");
+      }
+      if (joueur && pfc == t[0]) {
+          console.log("Perdu");
+      }
+      if (joueur && pfc == t[1]) {
+          console.log("Gagner");
+      }
+    });
 });
